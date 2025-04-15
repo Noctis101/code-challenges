84e50478c077f28566364b360acf96784d544046
Michael G. Bryan III <mikeybryan2310@gmail.com>
2025-04-15 18:24:51 -0500

    Solution, 11 tests passed

--- wellness-centre/src/main/java/org/codechallenges/Main.java
+++ wellness-centre/src/main/java/org/codechallenges/Main.java
@@ -1,5 +1,6 @@
 package org.codechallenges;
 
+import java.util.Arrays;
 /**
  * Code Challenge - Wellness Centre Patient Walk-in's
  * <p>
@@ -8,10 +9,30 @@ package org.codechallenges;
 public class Main {
 
     public static int solution(int N, int[][] patient) {
-        return 0;
+        int nextFreeTime = 0;
+        int cumulativeTimeWaited = 0;
+        for (int i = 0; i < N; i++) {
+            //Doctor is Free:
+            if ( patient[i][0] >= nextFreeTime) {
+                nextFreeTime = patient[i][0];
+            }
+            int endTime = nextFreeTime + patient[i][1];
+            int patientWaitTime = endTime - patient[i][0];
+            nextFreeTime = endTime;
+            cumulativeTimeWaited += patientWaitTime;
+
+            // For debugging:
+            System.out.println("Patient " + Arrays.toString(patient[i]) +
+                    " - Arrival time: " + patient[i][0] +
+                    " - Time needed: " + patient[i][1] +
+                    " - Waiting time: " + patientWaitTime +
+                    ", Next free time: " + nextFreeTime);
+            patientWaitTime = 0;
+        }
+
+        System.out.println("Cumulative Waiting Time: " + cumulativeTimeWaited);
+        System.out.println("Average Waiting Time: ");
+        return (int) (cumulativeTimeWaited / N);
     }
 
-    public static void main(String[] args) {
-        System.out.println("Use the unit tests to verify your solution.");
-    }
 }
